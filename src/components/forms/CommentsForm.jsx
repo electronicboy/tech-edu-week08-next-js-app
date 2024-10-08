571ba9fc4b4f298eccfca2b404711cb283fcc135
Shane Freeder <theboyetronic@gmail.com>
2024-10-08 14:55:39 +0100

    Fix counting skills

--- src/components/forms/CommentsForm.jsx
+++ src/components/forms/CommentsForm.jsx
@@ -22,9 +22,9 @@ export default function CommentsForm({mediaID, submitAction}) {
             <FormSelect name={"rating"}>
                 <option value={1}>1</option>
                 <option value={2}>2</option>
-                <option value={3}>4</option>
-                <option value={4}>5</option>
-                <option value={5}>6</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
             </FormSelect>
             <FormButton text={"Add comment"}/>
         </ClientFormElement>
